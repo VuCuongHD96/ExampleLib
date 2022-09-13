ecb20d6290b8f2858a69c23848253995fc272d87
cuongvx <cuongvx@mobio.vn>
2022-09-13 10:07:52 +0700

    build carthar

---
+++ Carthage/Build/ExampleLib.xcframework/ios-arm64_x86_64-simulator/ExampleLib.framework/Headers/ExampleLib.h
@@ -0,0 +1,19 @@
+//
+//  ExampleLib.h
+//  ExampleLib
+//
+//  Created by Sun on 7/13/21.
+//  Copyright © 2021 Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExampleLib.
+FOUNDATION_EXPORT double ExampleLibVersionNumber;
+
+//! Project version string for ExampleLib.
+FOUNDATION_EXPORT const unsigned char ExampleLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExampleLib/PublicHeader.h>
+
+
